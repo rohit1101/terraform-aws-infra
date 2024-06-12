744b81ddb8df7697bf2d451935b4d6f24e2cce36
Rohit <rohit@Rohits-Air-2.lan>
2024-06-12 15:28:36 +0530

    vm setup

--- main.tf
+++ main.tf
@@ -243,6 +243,7 @@ resource "aws_instance" "docker_vm" {
   
   
   # install docker
+  apt-get update -y
   apt-get install ca-certificates curl
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -251,8 +252,7 @@ resource "aws_instance" "docker_vm" {
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update -y
-  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  
 EOF
 
   tags = {
